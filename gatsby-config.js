1ee2c01e3cfea084aec6a49e84ef576d72aa68f0
Dustin Knopoff <dustinknopoff@gmail.com>
2020-01-02 12:42:51 -0500

    feat(favicon support):

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,6 +66,10 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-plugin-favicon`,
+      options: { logo: "./src/img/logo.svg" }
+    },
+    {
       resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "gatsby";
-import github from "../img/github-icon.svg";
-import logo from "../img/logo.svg";
 import Logo from "./logo";
 
 const Navbar = class extends React.Component {
